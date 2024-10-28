182560aa11c8ed5db2394735f30921b3de5e3b36
Dev Arome <moses21429@gmail.com>
2024-10-28 08:26:59 +0100

    runtime

--- app/index.tsx
+++ app/index.tsx
@@ -21,8 +21,8 @@ export default function Index() {
   };
 
   useEffect(() => {
-    //setTimeout(checkData, 3000);
+    setTimeout(checkData, 3000);
   }, []);
 
-  return <Redirect href="/forgot-password/reset-password" />;
+  return <Redirect href="/splashscreen" />;
 }
